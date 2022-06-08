18ef37b3c53e7ed308f065b72bb06cdaff0d8f25
Christopher Kolb <CkAstro.git@gmail.com>
2022-06-08 13:21:10 -0400

    scene controller is hidden when no items are present

--- client/src/components/display/sceneoptions.js
+++ client/src/components/display/sceneoptions.js
@@ -1,10 +1,13 @@
+import { useRenderables } from '../../contexts/renderables';
 import { useCamera } from '../../contexts/camera';
 import './display.css';
 
 const SceneController = ({ cameraId }) => {
+   const { renderables } = useRenderables();
+   const isHidden = renderables.length > 0 ? '' : 'hidden';
 
    return (
-      <div className='sceneController'>
+      <div className={`sceneController ${isHidden}`}>
          <div><div>&#171;</div></div>
          <div>1 of 1</div>
          <div><div>&#187;</div></div>
