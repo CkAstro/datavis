a1ee1310f2d74b38e36f0cbb919e43e18162f9d8
Christopher Kolb <CkAstro.git@gmail.com>
2022-07-07 19:34:41 -0400

    stopping click propagation so modal may be interacted with

--- client/src/components/modal/index.js
+++ client/src/components/modal/index.js
@@ -1,10 +1,11 @@
 import style from './modal.module.css';
 
 const Modal = ({ children, closeModal, isActive }) => {
+   const handleClick = event => event.stopPropagation();
 
    return (
       <div className={`${style.modalContainer} ${isActive ? style.active : ''}`} onClick={closeModal}>
-         <div className={style.modal}>
+         <div className={style.modal} onClick={handleClick}>
             {children}
          </div>
       </div>
