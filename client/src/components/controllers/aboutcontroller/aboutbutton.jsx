b29cf3a47a9cb6ab175f1314fccad29d321e59bd
Chris <cekolb.astro@gmail.com>
2022-08-05 16:36:19 -0400

    style update

--- client/src/components/controllers/aboutcontroller/aboutbutton.jsx
+++ client/src/components/controllers/aboutcontroller/aboutbutton.jsx
@@ -4,10 +4,12 @@ import { Button, Icons } from "components/elements";
 const ModalContent = () => (
    <div>
       <h1>About</h1>
+
+      <p>Welcome to DataVis: a web-based 3D volumetric data visualization tool.</p>
       
       <p>The <b>front end</b> is created using React and a custom WebGL/GLSL implementation to quickly render volumetric data. A serverless 'offline' version is in the works.</p>
 
-      <p>The <b>back end</b> runs on node+Express and will eventually process,store (upon request), and re-serve user-submitted data in an easily-renderable data format. A user log-in is planned so both saved sessions and stored datasets are accessable from anywhere.</p>
+      <p>The <b>back end</b> runs on node+Express and will eventually process, store (upon request), and then serve user-submitted volumetric data in an easily-renderable data format. A user log-in is planned so both saved sessions and stored datasets are accessable from anywhere.</p>
    </div>
 );
 
